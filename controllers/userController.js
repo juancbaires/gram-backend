75ebd609b70b65ff8c35594a301e412a517ef82f
Juan Baires <>
2019-12-29 11:40:53 -0500

    created controller functions to create, login, logout, udpate and delete users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,71 +1,118 @@
 const User = require("../models/User");
 require("mongoose");
-// const passport = require("../auth/passport");
-// const config = require("../auth/config");
 const bcrypt = require("bcrypt-nodejs");
 
 // post request to signup
 exports.create = async function(req, res) {
+  const user = new User();
   try {
-    const AlreadyExists = await User.find({ username: req.body.username });
-    if (AlreadyExists.length === 0) {
-      const user = new User({
-        username: req.body.username,
-        password: bcrypt.hashSync(
-          req.body.password,
-          bcrypt.genSaltSync(8),
-          null
-        )
-      });
-      await user.save();
-      let payload = {
-        username: user.username,
-        posts: user.posts
-      };
-      const token = jwt.encode(payload, config.jwtSecret);
-      res.status(201).send("User created", { token });
-    } else {
-      return res
-        .status(404)
-        .send("Username already taken! Try again with different username");
-    }
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
+
+// login function
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.username,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// logout function (out of current device)
+exports.logout = async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
   } catch (error) {
     res.status(500).send(error);
   }
 };
 
-// index the home page make every user available
-exports.index = (req, res) => {
-  User.find({})
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res.status(400).send("Unable to load home page. Please try again!");
-    });
+// logout of all devices erase all stored tokens
+exports.logoutAll = async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
-exports.login = async (req, res) => {
-  if (req.body.username && req.body.password) {
-    try {
-      const user = await User.findOne({ username: req.body.username });
-      console.log(user);
-      if (user.validPassword(req.body.password)) {
-        let payload = {
-          username: user.username,
-          posts: user.posts
-        };
-        const token = jwt.encode(payload, config.jwtSecret);
-        res.send({ token });
-      } else {
-        res.status(404).send("Try another password/username");
-      }
-    } catch (error) {
-      res.status(500).send(error.message);
+// See users own profile
+exports.getOwnProfile = async (req, res) => {
+  res.send(req.user);
+};
+
+// this is found by ID. To see other profiles etc..
+exports.GetUser = async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
     }
-  } else {
-    return send
-      .status(404)
-      .send({ error: "Username and password are required fields!" });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// User patch to udpate a user. Also using _id params
+exports.updateUser = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["username", "password"];
+  const isValid = updates.every(update => {
+    allowedUpdates.includes(updates.toLowerCase());
+  });
+
+  if (!isValid) {
+    res.status(400).send({ error: "Invalid Updates" });
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    updates.forEach(update => (user[update] = req.body[update]));
+
+    await user.save();
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+// delete user by ID
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
   }
 };
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
-const mongoose = require("../db/connection.js");
 const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //TODO * passwords
 // const bcrypt = require('bcrypt-nodejs')
@@ -8,6 +10,7 @@ const validator = require("validator");
 const User = new mongoose.Schema({
   username: {
     type: String,
+    required: true,
     trim: true
   },
   password: {
@@ -22,4 +25,36 @@ const User = new mongoose.Schema({
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 });
 
+// function to hash password before saving
+User.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+// Generate auth token
+User.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "thisisgram");
+  user.token = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+// Find by credentials middleware to find user quicker
+User.statics.FindByCredentials = async (username, password) => {
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error("unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Wrong paswords/unable to login");
+  }
+
+  return user;
+};
 module.exports = mongoose.model("User", User);
