7e4cdd801b8a9d8c1c9a2291e4de0d894ec5ebfb
Juan Baires <>
2020-01-25 13:00:14 -0500

    added controller functions for posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 const Post = require("../models/Post");
+const mongoose = require("mongoose");
 
 // createt a post
 exports.create = async (req, res) => {
@@ -14,6 +15,8 @@ exports.create = async (req, res) => {
   }
 };
 
+// requires auth middleware
+// Get all posts to a single user
 exports.getPosts = async (req, res) => {
   try {
     await req.user.populte("posts").execPopulate();
@@ -22,3 +25,46 @@ exports.getPosts = async (req, res) => {
     res.send(500).send(error.message);
   }
 };
+// Get a single post from a user
+// requires auth middleware
+exports.getSinglePost = async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const post = await Post.findOne({ _id, owner: req.user._id });
+    if (!post) {
+      return res.status(404).send("Task unavailable!");
+    }
+    res.status(200).send(post);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// update a single post
+// requires auth middleware
+exports.updatePost = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["content", "timeStamp", "img", "comments"];
+  const isValid = updates.every(update => {
+    allowedUpdates.includes(update);
+  });
+
+  if (!isValid) {
+    return res.status(404).send({ error: "invalid update!" });
+  }
+
+  try {
+    const _id = req.params.id;
+    const post = await Post.findOne({ _id, owner: req.user._id });
+    updates.forEach(udpate => (post[update] = req.body[update]));
+    await post.save;
+    res.send(post);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// delete a post
+// requires auth middleware
+
+exports.deletePost = async (req, res) => {};
